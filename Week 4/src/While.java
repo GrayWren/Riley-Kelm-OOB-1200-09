1e9c35887e830b3cd13ce835cedefc9dc80e6423
GrayWren <rileykelm2002@gmail.com>
2025-01-29 12:22:52 -0500

    error fixing

---
+++ Week 4/src/While.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class While {
+    private static Scanner input = new Scanner((System.in));
+
+    public static void main(String[] args) {
+        System.out.println("Please input range");
+        int i = input.nextInt();
+       /* int n = 0;
+       /* while (n < i) {
+            if (n % 2 == 0) {
+                System.out.println("Its even!");
+            } else {
+                System.out.println("Its odd");
+            }*/
+
+            /*do {
+                System.out.println(i);
+                i++;
+            } while (i < 8);*/
+            for(int x=0; x<i; x++){
+                System.out.println(x);
+            }
+
+        }
+
+
+    }
