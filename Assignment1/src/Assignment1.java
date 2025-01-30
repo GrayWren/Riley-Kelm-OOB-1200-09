568cc1d2a9f30ce5c14ed2156974b787a655cad0
GrayWren <rileykelm2002@gmail.com>
2025-01-30 10:49:59 -0500

    Height input added

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Assignment1 {
+    private static Scanner input = new Scanner((System.in));
     public static void main(String[] args) {
-        
+        boolean height_checker=false;
+        boolean weight_checker=false;
+
+        while (height_checker==false){
+                System.out.println("Please input your height: ");
+                Float height = input.nextFloat();
+
+                if (24>height || height>120){
+                    System.out.println("Please input a height between 24 and 120 inches");
+                    input.nextFloat();
+                } else {
+                    height_checker=true;
+                    System.out.println("pog");
+            }
+        }
+        while (weight_checker==false){
+            System.out.println("Please input your weight: ");
+            Float weight = input.nextFloat();
+
+            if ()
+        }
+
+
+
+
     }
 }
