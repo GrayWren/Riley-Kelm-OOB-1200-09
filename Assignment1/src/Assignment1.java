5fd38c7fb0c991453cac9945ec51d7ed96cc9a62
GrayWren <rileykelm2002@gmail.com>
2025-01-30 15:18:28 -0500

    Comments added

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
-
+/*Name: Riley Kelm
+Date: January 30th 2025
+Description: Takes input from the user finding their weight and height and using that to find their BMI*/
 public class Assignment1 {
     private static Scanner input = new Scanner((System.in));
     public static void main(String[] args) {
         boolean height_checker=false;
         boolean weight_checker=false;
         System.out.println("Please input your height: ");
-        Float height = input.nextFloat();
-        while (height_checker==false){
-           
-            if (24>height || height>120){
+        Float height = 1.2f;
+        while (height_checker==false) {
+            height = input.nextFloat();
+            if (24 > height || height > 120) {
                 System.out.println("Please input a height between 24 and 120 inches");
-                height=input.nextFloat();
             } else {
-                height_checker=true;
-
+                height_checker = true;
             }
-
-
         }
+
         System.out.println("Please input your weight: ");
-        Float weight = input.nextFloat();
+        Float weight = 0f;
         while (weight_checker==false){
+            weight=input.nextFloat();
+
             if (weight<25){
                 System.out.println("Please input a weight that is greater then 25 pounds: ");
-                weight=input.nextFloat();
+
             }else {
                 weight_checker=true;
                 System.out.println("pag");
             }
         }
+
+
     Float bmi=(weight/(height*height))*703;
         if (bmi<16){
             System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you severally underweight",weight,height,bmi);
