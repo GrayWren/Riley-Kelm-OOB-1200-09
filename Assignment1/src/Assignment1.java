dbd3bd27d1857abf9e2e7ca2278db65570b3ee04
GrayWren <rileykelm2002@gmail.com>
2025-01-30 10:57:41 -0500

    weight input added

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -5,24 +5,27 @@ public class Assignment1 {
     public static void main(String[] args) {
         boolean height_checker=false;
         boolean weight_checker=false;
-
+        System.out.println("Please input your height: ");
+        Float height = input.nextFloat();
         while (height_checker==false){
-                System.out.println("Please input your height: ");
-                Float height = input.nextFloat();
-
                 if (24>height || height>120){
                     System.out.println("Please input a height between 24 and 120 inches");
-                    input.nextFloat();
+                    height=input.nextFloat();
                 } else {
                     height_checker=true;
                     System.out.println("pog");
             }
         }
+        System.out.println("Please input your weight: ");
+        Float weight = input.nextFloat();
         while (weight_checker==false){
-            System.out.println("Please input your weight: ");
-            Float weight = input.nextFloat();
-
-            if ()
+            if (weight<25){
+                System.out.println("Please input a weight that is greater then 25 pounds: ");
+                weight=input.nextFloat();
+            }else {
+                weight_checker=true;
+                System.out.println("pag");
+            }
         }
 
 
