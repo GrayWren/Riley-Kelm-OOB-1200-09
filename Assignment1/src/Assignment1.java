0b9031510c645957c8055f011209b8b42a07e994
GrayWren <rileykelm2002@gmail.com>
2025-01-30 11:17:49 -0500

    Bmi working

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -8,6 +8,8 @@ public class Assignment1 {
         System.out.println("Please input your height: ");
         Float height = input.nextFloat();
         while (height_checker==false){
+
+
                 if (24>height || height>120){
                     System.out.println("Please input a height between 24 and 120 inches");
                     height=input.nextFloat();
@@ -27,8 +29,21 @@ public class Assignment1 {
                 System.out.println("pag");
             }
         }
+    Float bmi=(weight/(height*height))*703;
+        if (bmi<16){
+            System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you severally underweight",weight,height,bmi);
+        } else if (bmi<18.5) {
+            System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you underwight",weight,height,bmi);
 
+        } else if (bmi<25) {
+            System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you healthy",weight,height,bmi);
 
+        } else if (bmi<30) {
+            System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you overweight",weight,height,bmi);
+
+        }else {
+            System.out.printf("with a weight of %.2f pounds and a height of %.2f You get a BMI of %.2f which makes you obese",weight,height,bmi);
+        }
 
 
     }
